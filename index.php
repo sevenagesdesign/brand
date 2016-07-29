37d626a3b12182ffd381917d52847478efe83a34
Max Azarcon <contact@maxazarcon.com>
2016-07-29 14:06:01 -0400

    Update main page
    Add sticky nav, footer content, body classes, images, js

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<!--[if lt IE 7]>
+    <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="">
+<![endif]-->
+<!--[if IE 7]>
+    <html class="no-js lt-ie9 lt-ie8" lang="">
+<![endif]-->
+<!--[if IE 8]>
+    <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32">
+       
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body>
+    <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+<header>
+    <div class="content">
+        <div class="brand">
+            <a href="index.html">
+                <img id="header-logo" src="img/brand/seven-ages-logo-linear.png" />
+            </a>
+        </div>
+        <div id="mobile-nav-toggle">
+        </div>
+        <nav class="main-nav">
+            <?php include('parts/nav.php'); ?>
+        </nav>
+    </div>
+</header>
+<nav id="mobile-nav">
+    <?php include('parts/nav.php'); ?>
+</nav>
+<div id="main">
+    <div id="top">
+        <div class="content">
+    
+        </div>
+    </div>
+</div>
+
+
+
+
+<footer>
+    <div class="content">
+        <div class="brand">
+            <img src="img/brand/seven-ages-logo-linear.png">
+        </div>
+        <div class="social">
+            <nav class="social-nav">
+                <ul>
+                    <li><a href="#"><img src="img/social/facebook.png"></a></li>
+                    <li><a href="#"><img src="img/social/twitter.png"></a></li>
+                    <li><a href="#"><img src="img/social/youtube.png"></a></li>
+                    <li><a href="#"><img src="img/social/vimeo.png"></a></li>
+                    <li><a href="#"><img src="img/social/instagram.png"></a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="copyright">
+            <p>Copyright &copy; 2015 - <?php echo(date('Y')); ?> &bull; Seven Ages Design</p>
+        </div>
+    </div>
+</footer>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/3.0.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.1.0.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 jQuery(function($) {
-
+    
+    // Set Global Variables
+    
+    var winheight = $(window).height(),
+        navheight = $('header').height();
+    
+    // Toggle Mobile Navigation
+    
+    $('#mobile-nav-toggle').click(function(){
+        $(this).toggleClass('open');
+        $('nav#mobile-nav').slideToggle();
+    });
+    
+    // Set Top Div Height
+    
+    $('div#top').css('height', winheight * 5.65);
+    
+    $(window).resize(function(){
+        var winheight = $(this).height(),
+            navheight = $('header').height();
+        
+        $('div#top').css('min-height', winheight * 5.65);
+    });
+    
+    $(window).on('scroll', function(e){
+        var top = $(this).scrollTop();
+        
+        if (top > 16) {
+            $('header, #mobile-nav').addClass('sticky');
+        } else {
+            $('header, #mobile-nav').removeClass('sticky');
+        }
+    });
 });
+
